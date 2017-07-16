83d6af8f3a5236ab077a9191e992e99b556f78db
William Enkey <bill.enkey5@gmail.com>
2017-07-16 17:04:11 -0600

    final?

--- acme/js/acme.js
+++ acme/js/acme.js
@@ -23,31 +23,32 @@ $(function()
 $("#mainnav").on("click", "a", function(event){
     var view = $(this).text();
     console.log(view);
+    if (view != "Home"){
     $.ajax({
         url: "js/acme.json",
         dataType: "json",
         //Change output based on which link is clicked
         success: function (data) {
             console.log(data[view]);
-            if (view != "Home"){
-                document.title = "ACME: " + view;
-                $("#title").html(view);
-                $("#description").html("DESCRIPTION: " + data[view].description);
-                    console.log(data[view].description);
-                $("#manufacturer").html("Manufactured by: " + data[view].manufacturer);
-                    console.log("Manufactured by: " + data[view].manufacturer);
-                $("#price").html("Cost: " + data[view].price);
-                    console.log("Cost: " + data[view].price);
-                $("#blank").html("Overall review: " + data[view].reviews + "/5");
-                    console.log("Overall review: " + data[view].reviews + "/5");
+            document.title = "ACME: " + view;
+            $("#title").html(view);
+            $("#description").html("DESCRIPTION: " + data[view].description);
+                console.log(data[view].description);
+            $("#manufacturer").html("Manufactured by: " + data[view].manufacturer);
+                console.log("Manufactured by: " + data[view].manufacturer);
+            $("#price").html("Cost: " + data[view].price);
+                console.log("Cost: " + data[view].price);
+            $("#blank").html("Overall review: " + data[view].reviews + "/5");
+            console.log("Overall review: " + data[view].reviews + "/5");
 
-                $("#picture").empty("img");
-                $("#picture").prepend("<img src='" + data[view].path + "' id = 'picture' alt='product image'>");
+            $("#picture").empty("img");
+            $("#picture").append("<img src='" + data[view].path + "' id = 'picture' alt='product image'>");
 
             //hide the main page shtuffs
-                $("#centerdiv").css("display", "none");
-                $("#bottomdiv").css("display", "none");
-            }
+            $("#centerdiv").css("display", "none");
+            $("#bottomdiv").css("display", "none");
         }
     });
+    }
+    else $("#mainnav ul").html("<li><a href='/acme/index.html'>Home</a></li>");
 });
